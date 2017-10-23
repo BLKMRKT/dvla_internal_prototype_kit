e8b46d753f26ed8d23cc4a1e41233122e2b69c2b
Jay Price <jayprice@blkmrkt.co.uk>
2017-10-23 15:30:54 +0100

    test

---
+++ app/assets/javascripts/schema.js
@@ -0,0 +1,64 @@
+var AFRL = {
+	manufacturers: [
+		{
+			name: 'Honda',
+			id: '928456',
+			contact: {
+				name: 'Liam',
+				email: 'liam@liam.com'
+			},
+			retailers: [
+				{
+					name: 'Honda dealer',
+					id: '2ertbr',
+					contact: {
+					name: 'Liam',
+					email: 'liam@liam.com'
+					},
+					users: [
+						{
+							name: 'Honda dealer',
+							id: '2ertbr',
+							email: 'liam@liam.com'
+						},
+						{
+							name: 'Honda dealer 2',
+							id: '2ertbr',
+							email: 'liam@liam.com'
+						}
+					]
+				}
+			]
+		},
+		{
+			name: 'Bentley',
+			id: '928456',
+			contact: {
+				name: 'Liam',
+				email: 'liam@liam.com'
+			},
+			retailers: [
+				{
+					name: 'Honda dealer',
+					id: '2ertbr',
+					contact: {
+						name: 'Liam',
+						email: 'liam@liam.com'
+					},
+					users: [
+						{
+							name: 'Honda3f2g4g dealer',
+							id: '2ertbr',
+							email: 'liam@liam.com'
+						},
+						{
+							name: 'Honda3rf dealer',
+							id: '2ertbr',
+							email: 'liam@liam.com'
+						}
+					]
+				}
+			]
+		}
+	]
+};
