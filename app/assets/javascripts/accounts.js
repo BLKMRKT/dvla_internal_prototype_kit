0c83814c0fb00f1433e007f9ed91addac91ec59e
Jay Price <jayprice@blkmrkt.co.uk>
2017-11-10 00:03:33 +0000

    Users accounts

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -3,13 +3,23 @@
 if (member.currentManufacturer !== undefined) {
 	var manufacturer = member.currentManufacturer;
 	var retailers = manufacturer.retailers;
+	var manufacturerUsers = manufacturer.users;
+	var currentUser = member.currentUser;
 
-	$(document).ready(function() {
-		$('.account-name').html(manufacturer.name);
-		$('.account-code').html('(' + manufacturer.id + ')');
-	});
-} 
-
-
-// Retailer list
+	$('.account-name').html(manufacturer.name);
+	$('.account-code').html(manufacturer.supplierCode);
+	$('.account-type').html(manufacturer.accountType);
+	$('.contact-name').html(manufacturer.contact.forenames + ' ' + manufacturer.contact.surnames);
+	$('.address').html(manufacturer.address1 + '<br>' + manufacturer.address2 + '<br>' + manufacturer.address3 + '<br>' + manufacturer.postCode);
+	$('.phone-number').html(manufacturer.contact.phoneNumber);
+	$('.email-address').html(manufacturer.contact.emailAddress);
+	$('.manufacturer-phone-number').html(manufacturer.phoneNumber);
+	$('.manufacturer-email-address').html(manufacturer.emailAddress);
+	$('.financial-responsibility').html(manufacturer.financialResponsibility);
+	$('.VAT-number').html(manufacturer.VATnumber);
+	$('.setup-date').html(manufacturer.accountCreation);
+	
+	$('.user-name').html(currentUser);
+	
+}
 
