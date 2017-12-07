e00411b1f8776221fd756b9b0d28791560f05d2e
Jay Price <jayprice@blkmrkt.co.uk>
2017-12-07 16:57:16 +0000

    Commit

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -1,55 +1,104 @@
-//var name = location.search.split('name=')[1];
+var accountcreation = location.search.split('accountcreation=')[1];
+
+
+// Account creation
+
+if (accountcreation == "true") {
+	$('#manufacturer-success').show();
+}
+
+if (accountcreation == "true") {
+	$('#retailer-success').show();
+}
+
+// Account actions
+
+$("#create-retailer").click(function(){
+	go('/account-creation/retailer-creation');
+});
 
 if (member.currentManufacturer !== undefined) {
-	var manufacturer = member.currentManufacturer;
-	var manufacturerUsers = manufacturer.users;
 	
+	var manufacturer = member.currentManufacturer;
+	var manufacturerUsers = manufacturer.users;	
 	var retailers = manufacturer.retailers;
-
 	var user = member.currentUser;
 	
 	// Manufacturer Data
-	$('.manufacturer-name').html(manufacturer.name);
-	$('.account-code').html(manufacturer.supplierCode);
-	$('.account-type').html(manufacturer.accountType);
-	$('#manufacturer-account-type').html(manufacturer.accountType);
-	$('.contact-name').html(manufacturer.contact.forenames + ' ' + manufacturer.contact.surnames);
-	$('.address').html(manufacturer.address1 + '<br>' + manufacturer.address2 + '<br>' + manufacturer.address3 + '<br>' + manufacturer.postCode);
-	$('.phone-number').html(manufacturer.contact.phoneNumber);
-	$('.email-address').html(manufacturer.contact.emailAddress);
-	$('.manufacturer-phone-number').html(manufacturer.phoneNumber);
-	$('.manufacturer-email-address').html(manufacturer.emailAddress);
-	$('.financial-responsibility').html(manufacturer.financialResponsibility);
-	$('.VAT-number').html(manufacturer.VATnumber);
-	$('.setup-date').html(manufacturer.accountCreation);
-	$('#manufacturer-status').html(manufacturer.accountStatus);
-	
-	// Manufacturer user data
-	$('.user-name').html(user.forenames + ' ' + user.surnames);
-	$('.user-id').html(user.id);
-	$('#staff-account-type').html(user.accountType);
-	$('.user-phone-number').html(user.phoneNumber);
-	$('.user-email').html(user.emailAddress);
-	$('.user-class').html(user.privacyClass);
-	$('.user-status').html(user.accountStatus);
-	$('.password-status').html(user.passwordStatus);
-	$('.security-q1-answer').html(user.securityQ1);
-	$('.security-q2-answer').html(user.securityQ2);
+//	$('.manufacturer-name').html(manufacturer.name);
+//	$('.account-code').html(manufacturer.supplierCode);
+//	$('.account-type').html(manufacturer.accountType);
+//	$('#manufacturer-account-type').html(manufacturer.accountType);
+//	$('.contact-name').html(manufacturer.contact.forenames + ' ' + manufacturer.contact.surnames);
+//	$('.address').html(manufacturer.address1 + '<br>' + manufacturer.address2 + '<br>' + manufacturer.address3 + '<br>' + manufacturer.postCode);
+//	$('.phone-number').html(manufacturer.contact.phoneNumber);
+//	$('.email-address').html(manufacturer.contact.emailAddress);
+//	$('.manufacturer-phone-number').html(manufacturer.phoneNumber);
+//	$('.manufacturer-email-address').html(manufacturer.emailAddress);
+//	$('.financial-responsibility').html(manufacturer.financialResponsibility);
+//	$('.VAT-number').html(manufacturer.VATnumber);
+//	$('.setup-date').html(manufacturer.accountCreation);
+//	$('#manufacturer-status').html(manufacturer.accountStatus);
+//	
+//	// Manufacturer user data
+//	$('.user-name').html(user.forenames + ' ' + user.surnames);
+//	$('.user-id').html(user.id);
+//	$('#staff-account-type').html(user.accountType);
+//	$('.user-phone-number').html(user.phoneNumber);
+//	$('.user-email').html(user.emailAddress);
+//	$('.user-class').html(user.privacyClass);
+//	$('.user-status').html(user.accountStatus);
+//	$('.password-status').html(user.passwordStatus);
+//	$('.security-q1-answer').html(user.securityQ1);
+//	$('.security-q2-answer').html(user.securityQ2);
 }
 
-if (member.currentRetailer !== undefined) {
-	var retailer = member.currentRetailer;
-	var retailerUsers = retailer.users;
-	
-	// Retailer data
-	$('#retailer-account-type').html(retailer.accountType);
-	$('.retailer-name').html(retailer.name);
-	$('.retailer-code').html(retailer.supplierCode);
-}
+	// Creating a retailer - account data
 
-if (user.passwordStatus === 'Locked') {
-	$('.password-status').removeClass('active-marker');
-	$('.password-status').addClass('locked-marker');	
-}
+	// Page context
+	$('.retailer-name').html(member.questions.retailer);
+
+	// Individual contact
+	$('.contact-name').html(member.questions.contactName);
+	$('.contact-email').html(member.questions.contactEmail);
+	$('.contact-phone').html(member.questions.contactPhone);
+	$('.contact-fax').html(member.questions.contactFax);
+	
+	// Retailer details
+	$('.retailer-name').html(member.questions.retailer);
+	$('.MVRIS-code').html(member.questions.mvrisCode);
+	$('.dealer-code').html(member.questions.dealerCode);
+	$('.full-address').html(member.questions.address1 + '<br>' + member.questions.address2 + '<br>' + member.questions.address3 + '<br>' + member.questions.postcode);
+	$('.system-type').html(member.questions.systemType);
+	$('.companies-house').html(member.questions.companiesHouse);
+	$('.VAT-number').html(member.questions.vatNumber);
+	$('.date-of-creation').html(member.questions.dateOfCreation);	
+
+
+	// Creating a retailer - account data
+
+	// Page context
+	$('.retailer-name').html(member.questions.retailer);
+
+//if (member.currentRetailer !== undefined) {
+//	var retailer = member.currentRetailer;
+//	var retailerUsers = retailer.users;
+//	
+//	// Retailer data
+//	$('#retailer-account-type').html(retailer.accountType);
+//	$('.retailer-name').html(retailer.name);
+//	$('.retailer-code').html(retailer.supplierCode);
+//}
+
+
+// Account status 
+
+//if (user.passwordStatus === 'Locked') {
+//	$('.password-status').removeClass('active-marker');
+//	$('.password-status').addClass('locked-marker');	
+//}
+
+
+
 
 
---
+++ app/assets/javascripts/schema-2.js
@@ -0,0 +1,149 @@
+var AFRL = {
+	manufacturers: [
+		{
+			name: 'Honda',
+			id: '928456',
+			accountType: 'Manufacturer',
+			address: {
+				line1: '123 Example Rd',
+				line2: 'Llansamlet',
+				line3: '',
+				city: 'Swansea',
+				postCode: 'SA1 9JK'
+			},
+			phoneNumber: '01792 220039',
+			emailAddress: 'admin@honda.co.uk',
+			supplierCode: 'BP',
+			accountStatus: 'Active',
+			financialResponsibility: 'Yes',
+			VATnumber: '12013291',
+			accountCreation: '1 November 2004',
+			contact: {
+				title: 'Dr',
+				forenames: 'Liam',
+				surnames: 'Betsworth',
+				id: '1201',
+				phoneNumber: '01792 698039',
+				emailAddress: 'liam.betsworth@honda.co.uk',
+			},
+			retailers: [
+				'9284'
+			],
+			users: [
+				'6543'
+			]
+		}
+	],
+	retailers: [
+		{
+			name: 'Honda Whitchurch',
+			manufacturer: 'honda',
+			id: '9284',
+			MVRIS: '123379',
+			accountType: 'Retailer',
+			address: {
+				line1: '10 Alexandra Rd',
+				line2: 'Whitchurch',
+				line3: '',
+				city: 'Cardiff',
+				postCode: 'CF24 4QR'
+			},
+			phoneNumber: '02920 698039',
+			emailAddress: 'honda.whitchurch@honda.co.uk',
+			supplierCode: 'B189',
+			accountStatus: 'Active',
+			financialResponsibility: 'Yes',
+			VATnumber: '12013291',
+			accountCreation: '1st November 2004',
+			contact: {
+				title: 'Mr',
+				forenames: 'Bobby',
+				surnames: 'Singh',
+				id: '1207',
+				accountType: 'Staff',
+				phoneNumber: '01792 698039',
+				emailAddress: 'bobby.signh@honda.co.uk',
+				privacyClass: 'Admin',
+				accountStatus: 'Active',
+				securityQ1: 'Butterworth',
+				securityQ2: '182901'
+			},
+			manufacturers: [
+				'928456'
+			],
+			users: [
+				'6543',
+				'26543'
+			]
+		}
+	],
+	users: [
+		{
+			id: '6543',
+			name: 'Jay Price',
+			phoneNumber: '01792 830857'
+		},
+		{
+			id: '26543',
+			name: 'Liam Price',
+			phoneNumber: '01792 832257'
+		}
+	]
+};
+
+
+// Get manufacturer by ID
+function getManufacturer(manufacturerID) {
+	for (var i = 0; i < AFRL.manufacturers.length; i++) {
+		if (AFRL.manufacturers[i].id == manufacturerID) {
+			return AFRL.manufacturers[i];
+		}
+	}
+}
+
+// Get manufacturer user by ID
+function getUser(userID) {
+	for (var i = 0; i < AFRL.users.length; i++) {
+		if (AFRL.users[i].id == userID) {
+			return AFRL.users[i];
+		}
+	}
+}
+
+// Get retailer by ID
+function getRetailer(retailerID) {
+	for (var i = 0; i < AFRL.retailers.length; i++) {
+		if (AFRL.retailers[i].id == retailerID) {
+			return AFRL.retailers[i];
+		}
+	}
+}
+
+// Get users of a manufacturer
+function getManufacturerUsers(manufacturer) {
+	return manufacturer.users.map(function(userID) {
+		return getUser(userID);
+	});
+}
+
+// Get retailers of a manufacturer
+function getManufacturerRetailers(manufacturer) {
+	return manufacturer.retailers.map(function(retailerID) {
+		return getRetailer(retailerID);
+	});
+}
+
+// Get users of a retailer
+function getRetailerUsers(retailer) {
+	return retailer.users.map(function(userID) {
+		return getUser(userID);
+	});
+}
+
+// Get manufacturers of a retailer
+function getRetailerManufacturers(retailer) {
+	return retailer.manufacturers.map(function(manufacturerID) {
+		return getManufacturer(manufacturerID);
+	});
+}
+
